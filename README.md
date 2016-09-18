e9458d1638db652d6160f7fbdc5c02ebc2cbe07d
Mike Hughes <me@mike-hughes.net>
2016-09-19 00:39:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Eth balance test
+
+To start the server:
+~~~npm start~~~
+
+Site available at http://localhost:3000/
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ethAddr = require('ethereum-address');
+const axios = require('axios');
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.post('/', (req, res) => {
+	getBalance(req.body.address)
+		.then((balance) => {
+			res.render('index', {
+					balance: balance,
+					address: req.body.address
+				}
+			);
+		})
+		.catch((err) => {
+			res.render('index', {
+					error: err,
+					address: req.body.address
+				}
+			);
+		});
+});
+
+
+var getBalance = async ((address) => {
+	if (!ethAddr.isAddress(address)) {
+		throw "Error: Address is not valid";
+	}
+
+	var {data} = await (axios.get(`https://api.blockcypher.com/v1/eth/main/addrs/${address}/balance`));
+
+	if (typeof data.balance !== 'undefined') {
+		return data.balance / 1000000000000000000;
+	} else {
+		throw "Error: No balance received";
+	}
+
+});
+
+const server = app.listen(3000, () => {
+	let {address, port} = server.address();
+	console.log(`Test listening on http://${address}:${port}`);
+});
